84f2c994009765d77c0d4c0b757edf99c1112a42
Kiril Ivanov <kiril_ivanov_04@abv.bg>
2023-02-05 22:40:32 +0200

    1.0.1
    Added icon + Native Splash sreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:duda_shelter/firebase_options.dart';
 import 'package:duda_shelter/pages/Auth/authPage.dart';
 import 'package:duda_shelter/pages/OnBoarding/onBoardPage.dart';
@@ -9,7 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:page_transition/page_transition.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,26 +30,7 @@ class MyApp extends StatelessWidget {
       title: 'Duda Shelter',
       theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: Colors.white, primaryColor: ourPrimaryColor),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      backgroundColor: ourPrimaryColor,
-      nextScreen: const LoadApp(),
-      splash: Image.asset(
-        "lib/assets/logo.png",
-        fit: BoxFit.cover,
-      ),
-      splashIconSize: 300,
-      splashTransition: SplashTransition.fadeTransition,
-      pageTransitionType: PageTransitionType.rightToLeft,
+      home: const LoadApp(),
     );
   }
 }
